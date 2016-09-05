fb4273d11c5516d092c711d471f1ee05e6b84732
jaychou3519 <45279200@qq.com>
2016-09-05 23:42:47 +0800

    jzfp
    jzfp

---
+++ jzfp/src/com/demo/jzfp/activity/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.demo.jzfp.activity;
+
+import com.demo.jzfp.R;
+
+
+public class RegisterActivity extends BaseActivity {
+
+	@Override
+	protected void setView() {
+		setContentView(R.layout.activity_register);
+		setTitleText("用户注册");
+		setOnback(this);
+	}
+
+	@Override
+	protected void initView() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initData() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
